3351231d1539f4e7082e32308d4bac40e045a14a
Nygen <nducminh002@gmail.com>
2023-03-22 10:46:52 +0100

    Change: applying variables, responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="relative z-10 flex h-full w-full flex-1 bg-black">
-      <div className=" grid flex-1 grid-cols-main grid-rows-main grid-areas-main [&>*]:bg-white gap-3 m-4">
-        <div className="MainPanel grid-in-a"></div>
-        <div className="SecondPanel grid-in-b"></div>
-        <div className="thirdPanel grid-in-c"></div>
+      <div className=" mx-auto mt-16 grid max-w-screen-xl flex-1 grid-cols-main grid-rows-main gap-y-24 gap-x-12 grid-areas-main [&>*]:bg-[var(--color-2)]">
+        <div className="MainPanel max-w-[var(--main-cont-width)] grid-in-a"></div>
+        <div className="SecondPanel max-w-[var(--main-cont-width)] grid-in-b"></div>
+        <div className="thirdPanel max-w-[var(--side-cont-width)] grid-in-c "></div>
       </div>
     </main>
   );
