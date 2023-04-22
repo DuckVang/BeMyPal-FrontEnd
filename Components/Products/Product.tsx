1bb715025a0144aca974f621c26386ddae34452e
Nygen <nducminh002@gmail.com>
2023-04-22 22:26:06 +0200

    Add: basic product structure

--- Components/Products/Product.tsx
+++ Components/Products/Product.tsx
@@ -5,14 +5,21 @@ export const Product: FC = () => {
   const user = createRandomUser();
 
   return (
-    <div className="Product w-32 h-24 ">
-      <div className="upper flex flex-row">
-        <div className="avatar">
-          <div className="w-16 h-16">
-            <img src={user.avatar} />
+    <div className="Product min-w-64 card-compact min-h-48 card-bordered card w-full ">
+      <figure>
+        <img src={user.avatar} />
+      </figure>
+
+      <div className="card-body">
+        <div className="upper flex flex-row justify-between">
+          <div className="title w-fit">{user.firstName + " " + user.lastName}</div>
+          <div className="rating">
+            <div className="mask mask-star bg-yellow-400"></div>
+            <div className="rate">4.5 - 5</div>
           </div>
         </div>
-        <div className="name">{user.firstName + " " + user.lastName}</div>
+        <div className="badge-primary badge">{user.subscriptionTier}</div>
+        <div className="card-normal">{user.description}</div>
       </div>
     </div>
   );
