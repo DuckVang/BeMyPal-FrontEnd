b34a2762ec9a05eeb83d2308e21e407655ad39d0
Nygen <nducminh002@gmail.com>
2023-04-23 22:08:25 +0200

    Add: themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,50 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  purge: {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      safelist: [/data-theme$/],
+    },
+  },
+  theme: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          // ** primary is purple
+          primary: "#a991f7" /* Primary color */,
+          "primary-focus": "#8462f4" /* Primary color - focused */,
+          "primary-content": "#ffffff",
+          //** for now no */
+          secondary: "#f6d860" /* Secondary color */,
+          "secondary-focus": "#f3cc30" /* Secondary color - focused */,
+          "secondary-content": "#ffffff",
+
+          accent: "#37cdbe" /* Accent color */,
+          "accent-focus": "#2aa79b" /* Accent color - focused */,
+          "accent-content": "#ffffff",
+
+          neutral: "#3d4451" /* Neutral color */,
+          "neutral-focus": "#2a2e37" /* Neutral color - focused */,
+          "neutral-content": "#ffffff",
+
+          "base-100": "#ffffff",
+          "base-200": "#f9fafb" /* Base color, a little darker */,
+          "base-300": "#d1d5db" /* Base color, even more darker */,
+          "base-content": "#1f2937",
+
+          info: "#2094f3" /* Info */,
+          success: "#009485" /* Success */,
+          warning: "#ff9900" /* Warning */,
+          error: "#ff5724" /* Error */,
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
