9afa0fec81a2906eb23990b2b079c46b692d8e66
Nygen <nducminh002@gmail.com>
2023-04-23 22:09:17 +0200

    Add: testing posts

--- Components/SlidePanel.tsx
+++ Components/SlidePanel.tsx
@@ -1,19 +1,16 @@
 import { FC } from "react";
 import Image from "next/image";
+import { faker } from "@faker-js/faker";
 export const SlidePanel: FC = () => {
   function handleSignUp() {}
 
-  const Services = [
-    {
-      img: "https://store-images.s-microsoft.com/image/apps.608.13510798887677013.5c7792f0-b887-4250-8c4e-4617af9c4509.bcd1385a-ad15-450c-9ddd-3ee80c37121a?q=90&w=480&h=270",
+  const Services = [];
+  for (let index = 0; index < 10; index++) {
+    Services.push({
+      img: faker.image.business(2000, 1000, true),
       link: "/",
-    },
-    {
-      img: "https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_616x353.jpg?t=1668125812",
-      link: "/",
-    },
-    { img: "none", link: "/" },
-  ];
+    });
+  }
 
   return (
     <div className="flex w-[var(--main-cont-width)] flex-col gap-12 ">
@@ -26,7 +23,7 @@ export const SlidePanel: FC = () => {
           })}
         </div>
       </div>
-      <div className="lower-panel h-1/5"> lower panel</div>
+      <div className="lower-panel h-1/5 bg-slate-400"> lower panel</div>
     </div>
   );
 };
