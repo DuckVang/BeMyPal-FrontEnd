50ebe36a5e6cef0382f1a411b611ac2094a5e6b6
Nygen <nducminh002@gmail.com>
2023-05-03 22:01:06 +0200

    Add: conditional rendering for register page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-'use client'
+"use client";
 
 import "./globals.css";
 import Header from "../Components/Header";
-
+import { usePathname, useRouter } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathName = usePathname();
+
   return (
     <html lang="en" data-theme="mytheme">
       <body className="h-screen min-h-screen bg-base-100">
-        <Header />
+        {pathName != "/Register" && <Header />}
 
         {children}
       </body>
