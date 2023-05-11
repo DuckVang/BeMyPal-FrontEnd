958e9176e14fa5b879b9e5dbb0b3c5810fb0690b
Nygen <nducminh002@gmail.com>
2023-05-11 19:31:22 +0200

    Add: head and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@
 import "./globals.css";
 import Header from "../Components/Header";
 import { usePathname, useRouter } from "next/navigation";
+import Head from "next/head";
+import { Montserrat } from "@next/font/google"
+
+const montserrat = Montserrat({ subsets: ["latin"],  variable: '--montserrat-font' });
 
 export default function RootLayout({
   children,
@@ -12,8 +16,12 @@ export default function RootLayout({
   const pathName = usePathname();
 
   return (
-    <html lang="en" data-theme="mytheme">
-      <body className="h-screen min-h-screen bg-base-100">
+    <html lang="en" data-theme="mytheme" className={montserrat.className} >
+      <Head>
+        <title>Be my pal</title>
+     
+      </Head>
+      <body className="h-screen min-h-screen bg-base-100 ">
         {pathName != "/Register" && <Header />}
 
         {children}
